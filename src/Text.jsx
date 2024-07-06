85a3b39a859ebebf8a1b34505377e5854712fed7
gabiiizinha <rodriguesgabrielle2501@gmail.com>
2024-07-06 12:07:33 -0300

    Primeiro commit da aula

---
+++ src/Text.jsx
@@ -0,0 +1,4 @@
+const Text = () =>{
+    return (<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ullam maxime hic at inventore quam, quibusdam adipisci aliquid eaque, alias odio obcaecati facilis ex quasi veniam ea earum eveniet iste.</p>)
+}
+export default Text
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Contato</li>
+                <li>Sobre</li>
+                
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
